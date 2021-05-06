85e163566225a1194faac3343c93f80a86208e2e
Igor Zalutski <izalutski@gmail.com>
2021-05-06 21:09:34 +0100

    Add connections handling

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Storing connections in memory just for demo purposes
+// In production a persistent storage like DynamoDB or Postgres should be used
+// Otherwise the service won't scale horizontally
+const connections = new Set();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
@@ -11,10 +16,14 @@ app.get('/test', (req, res) => {
 });
 
 app.post('/connect', (req, res) => {
+    const connectionId = req.body.connectionId;
+    connections.add(connectionId);
     res.send('Connect');
 });
 
 app.post('/disconnect', (req, res) => {
+    const connectionId = req.body.connectionId;
+    connections.delete(connectionId);
     res.send('Disconnect');
 });
 
