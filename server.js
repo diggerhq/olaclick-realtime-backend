d9fdcce2e065586db13ff94a1f35789ac958ccfa
Igor Zalutski <izalutski@gmail.com>
2021-05-06 22:20:45 +0100

    Add gateway client

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
+const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require("@aws-sdk/client-apigatewaymanagementapi");
+
 const app = express();
 const port = 3000;
 
 // Storing connections in memory just for demo purposes
 // In production a persistent storage like DynamoDB or Postgres should be used
 // Otherwise the service won't scale horizontally
-const connections = new Set();
+const connections = new Set(['e7NGKfNFoAMCLeg=']);
+
+const awsClient = new ApiGatewayManagementApiClient({
+    region: 'us-east-1',
+    serviceId: 'mnv8w06et6'
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/test', (req, res) => {
+    connections.forEach(connectionId => {
+        const gatewayCommand = new PostToConnectionCommand({
+            ConnectionId: connectionId,
+            Data: 'test'
+        });
+        try {
+            awsClient.send(gatewayCommand);
+        } catch (error) {
+            console.error(error);
+        }
+    });
     res.send('Test');
 });
 
