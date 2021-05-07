e552d4e8a41905edba8b2da2ae1e761d1839d001
Igor Zalutski <izalutski@gmail.com>
2021-05-07 16:57:29 +0100

    Websockets with ws

--- server.js
+++ server.js
@@ -1,63 +1,30 @@
 const express = require('express');
-const { ApiGatewayManagementApiClient, PostToConnectionCommand } = require("@aws-sdk/client-apigatewaymanagementapi");
-const https = require('https');
-const aws4  = require('aws4');
+const cors = require('cors');
+const WebSocket = require('ws');
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-// Storing connections in memory just for demo purposes
-// In production a persistent storage like DynamoDB or Postgres should be used
-// Otherwise the service won't scale horizontally
-const connections = new Set(['e7NGKfNFoAMCLeg=']);
-const gatewayId = 'mnv8w06et6';
-
-const awsClient = new ApiGatewayManagementApiClient({
-    region: 'us-east-1',
-    serviceId: 'mnv8w06et6'
-});
+const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    const numClients = wsServer.clients.size;
+    res.send(`Connected clients: ${numClients}`);
 });
 
-app.get('/test', (req, res) => {
-    connections.forEach(connectionId => {
-        const gatewayCommand = new PostToConnectionCommand({
-            ConnectionId: connectionId,
-            Data: 'test'
-        });
-        const options = {
-            method: 'POST',
-            host: `${gatewayId}.execute-api.us-east-1.amazonaws.com/production/@connections`,
-            path: `/${connectionId}`,
-            service: 'apigateway',
-            region: 'us-east-1'
-        };
-        aws4.sign(options);
-        https.request(options, (res) => {
-            console.log(res);
-        });
+app.post('/broadcast', (req, res) => {
+    //TODO use SNS for horizontal scalability
+    wsServer.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            const data = req.body.message;
+            client.send(data);
+        }
     });
-    res.send('Test');
+    res.send(200);
 });
 
-app.post('/connect', (req, res) => {
-    const connectionId = req.body.connectionId;
-    connections.add(connectionId);
-    res.send('Connect');
+const server = app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.post('/disconnect', (req, res) => {
-    const connectionId = req.body.connectionId;
-    connections.delete(connectionId);
-    res.send('Disconnect');
-});
-
-app.post('/default', (req, res) => {
-    res.send('Default');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const wsServer = new WebSocket.Server({ server });
