b72feee2f5e24c7e12c3b48c52cdeea4eb7de44c
Igor Zalutski <izalutski@gmail.com>
2021-05-07 19:38:42 +0100

    Add redis

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const WebSocket = require('ws');
+const redis = require('redis');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 
+const publisher = redis.createClient('//ola-dev-redis.ewcbvl.ng.0001.use1.cache.amazonaws.com:6379');
+const subscriber = publisher.duplicate();
+const broadcastChannel = 'ws:broadcast';
+
 app.get('/', (req, res) => {
     const numClients = wsServer.clients.size;
     res.send(`Connected clients: ${numClients}`);
 });
 
-app.post('/broadcast', (req, res) => {
-    //TODO use SNS for horizontal scalability
-    wsServer.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            const data = req.body.message;
-            client.send(data);
-        }
-    });
+app.get('/broadcast', (req, res) => {
+    publisher.publish(broadcastChannel, 'test');
     res.send(200);
+});
+
+subscriber.on('message', (channel, message) => {
+    if (channel === broadcastChannel) {
+        wsServer.clients.forEach(wsClient => {
+            if (wsClient.readyState === WebSocket.OPEN) {
+                wsClient.send(message);
+            }
+        });
+    }
 });
 
 const server = app.listen(port, () => {
